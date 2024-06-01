0a99436f4eb1fef3e54c9f818ad192257cc44018
Lisandro Martinez <lisandromartz@gmail.com>
2024-05-31 22:26:42 -0300

    Set initial project files

---
+++ CDyM-TP03/main.c
@@ -0,0 +1,19 @@
+/*
+ * CDyM-TP03.c
+ *
+ * Created: 31/5/2024 21:23:05
+ * Author : Estanislao Carrer, Fernando Ramirez, Lisandro Martinez
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
